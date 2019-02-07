2c87182baf6afb21a31efbb16be77aa466e299dd
luis <luispcosta18@gmail.com>
2019-02-07 14:14:38 +0000

    load the railtie, but dont start cassandra when booting it up on rails

--- lib/cassandra_migrations.rb
+++ lib/cassandra_migrations.rb
@@ -4,3 +4,5 @@ require 'cassandra_migrations/errors'
 require 'cassandra_migrations/cassandra'
 require 'cassandra_migrations/migrator'
 require 'cassandra_migrations/migration'
+
+require 'cassandra_migrations/railtie' if defined?(Rails)
